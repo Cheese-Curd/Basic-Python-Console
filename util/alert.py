5586600a75f6b0b8f9a7cd78ddb4a34e9f46d77c
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2023-03-13 09:48:08 -0400

    More settings

--- util/alert.py
+++ util/alert.py
@@ -1,17 +1,25 @@
 from util.tColors import tColors # Get tColors Class
 import winsound # Windows Sounds
 
-def alert(type=-1, msg="N/A"):
-    if type == 1: # Success Alert
-        print(f"{tColors.ACCEPTED}SUCCESS: " + msg + f"{tColors.DEFAULT}")
-    elif type == 0: # Warning Alert
-        print(f"{tColors.WARNING}WARNING: " + msg + f"{tColors.DEFAULT}")
-    elif type == -1:
-        print(f"{tColors.WARNING}ALERT: " + msg + f"{tColors.DEFAULT}")
-    else: # Error/Unknown Alert
-        print(f"{tColors.ERR}ERR: " + msg + f"{tColors.DEFAULT}")
-    if config["sounds"]:
-       msgBeep(type)
+def alert(type=-1, msg="N/A", sound=False, prefix=True):
+	if prefix:
+	    if type == 1: # Success Alert
+	        print(f"{tColors.ACCEPTED}SUCCESS: " + msg + f"{tColors.DEFAULT}")
+	    elif type == 0: # Warning Alert
+	        print(f"{tColors.WARNING}WARNING: " + msg + f"{tColors.DEFAULT}")
+	    elif type == -1:
+	        print(f"{tColors.WARNING}ALERT: " + msg + f"{tColors.DEFAULT}")
+	    else: # Error/Unknown Alert
+	        print(f"{tColors.ERR}ERR: " + msg + f"{tColors.DEFAULT}")
+	else: # Before, I was using prints. With this,
+		if type == 1: # Success Alert
+	        print(f"{tColors.ACCEPTED}" + msg + f"{tColors.DEFAULT}")
+	    elif type == (0 or -1): # Warning Alert
+	        print(f"{tColors.WARNING}" + msg + f"{tColors.DEFAULT}")
+	    else: # Error/Unknown Alert
+	        print(f"{tColors.ERR}" + msg + f"{tColors.DEFAULT}")
+	if config["sounds"] and sound:
+	       msgBeep(type)
 
 def msgBeep(type):
     if type == 1: # Success Beep
